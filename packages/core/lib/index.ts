fef315214d9d2de340383128dae62bd84e139384
Keylenn <darklight_on@163.com>
2024-12-24 11:05:27 +0800

    chore: update pkg

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -3,7 +3,7 @@ import createBox from "./createBox"
 
 export type ProtectedBox = ReturnType<typeof createBox>
 export type BoxData<T extends ProtectedBox> = ReturnType<T['getData']>
-export type GetSnapshot<T extends ProtectedBox, D = BoxData<T>> = (data: D) => any
+export type Selector<T extends ProtectedBox, D = BoxData<T>> = (data: D) => any
 
 
 export default createBox

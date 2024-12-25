a3df2ca77dcbb57fc09c4bc2ba11792468656136
Keylenn <darklight_on@163.com>
2024-12-25 10:52:40 +0800

    fix: no packages

--- packages/react/lib/index.ts
+++
@@ -1,8 +0,0 @@
-import useSyncBoxStore from "./useSyncBoxStore";
-
-export default useSyncBoxStore;
-export { default as curryUseSyncBoxStore } from "./curryUseSyncBoxStore";
-export { default as shallowEqual } from "./util/shallowEqual";
-
-export type { ProtectedBox, BoxData, Selector } from "@boxly/core";
-export type { UseSyncBoxStoreOption } from "./useSyncBoxStore";
